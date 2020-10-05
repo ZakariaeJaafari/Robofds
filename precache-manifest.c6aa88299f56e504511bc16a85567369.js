7f20f897417611bd45ea9abf9e16f530ae52e0b4
zackinsea <zjaafari@insea.ac.ma>
2020-10-05 12:25:48 +0100

    Updates

---
+++ precache-manifest.c6aa88299f56e504511bc16a85567369.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee615cfa837d700df449ce127bec9fce",
+    "url": "/Robofds/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Robofds/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4628166c9e4b752342f3",
+    "url": "/Robofds/static/css/main.f593b237.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Robofds/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofds/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4628166c9e4b752342f3",
+    "url": "/Robofds/static/js/main.5c0ab158.chunk.js"
+  },
+  {
+    "revision": "804dac67586241c98171",
+    "url": "/Robofds/static/js/runtime-main.e88cec82.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofds/static/media/SEGA.12a3db8b.woff"
+  }
+]);
