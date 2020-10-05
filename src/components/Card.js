51cabb89c86b7aa3a890208b9d47f9b6e47ba8c0
zackinsea <zjaafari@insea.ac.ma>
2020-10-05 12:38:29 +0100

    cards to blue

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className='tc grow bg-dark-red br1 pa3 ma2 dib bw2 shadow-5'>
+    <div className='tc grow bg-dark-blue br1 pa3 ma2 dib bw2 shadow-5'>
       <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
       <div>
         <h2>{name}</h2>
